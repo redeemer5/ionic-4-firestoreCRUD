928509eb659e8b5e8e02ea21409c75591691a9f4
redeemer <redeemermatshili@thedigitalacademy.co.za>
2019-07-17 11:37:13 +0200

    random number added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Song } from '../models/song.interface';
 import { FirestoreService } from '../services/data/firestore.service';
 import { Router } from '@angular/router';
+import {NavController} from '@ionic/angular';
 
 
 @Component({
@@ -13,15 +14,22 @@ import { Router } from '@angular/router';
 export class HomePage implements OnInit {
 
 public songList;
+
+// random number stuff
+randomNumber:number;
+
+
 constructor(
   private firestoreService: FirestoreService,
   private router: Router,
+  private navctrl : NavController,
 ) {}
 
 
 ngOnInit()
 {
   this.songList = this.firestoreService.getSongList().valueChanges();
+  this.randomNumber = Math.floor(Math.random() * 100);
 }
 
 
